efe0e20b33293e24b8cd6031fd9d5c8c8309a878
Ramiro Urteaga <urteagaramiro33@gmail.com>
2025-09-10 18:21:39 -0300

    css

--- src/types/CardExample.Types.ts
+++
@@ -1,8 +0,0 @@
-import { StaticImageData } from "next/image";
-
-
-export interface CardExampleProps {
-    service: string;
-    description: string;
-    ico: StaticImageData | string;
-}
--- src/types/Hero.Types.ts
+++
@@ -1,7 +0,0 @@
-import { StaticImageData } from "next/image";
-
-export interface HeroProps {
-  ColorAngle: string;
-  CarAngle: StaticImageData;
-  CoverHero: StaticImageData; 
-}
