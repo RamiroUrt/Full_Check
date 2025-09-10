7ab6985c9508217dfc97a071ce4217363a5dd0af
Ramiro Urteaga <urteagaramiro33@gmail.com>
2025-09-10 18:30:31 -0300

    delete

--- src/types/CardParts.Types.ts
+++
@@ -1,8 +0,0 @@
-import { StaticImageData } from "next/image";
-
-export interface cardPartsProps {
-  title: string;
-  car: 'car' | 'truck' | 'van';
-  img?: StaticImageData | string;
-  price: number;
-}
