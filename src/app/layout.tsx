cd7b03dc4a1a94d9b5f6faaf1f7d0d7c93e8eaa3
Ramiro Urteaga <urteagaramiro33@gmail.com>
2025-12-04 15:22:46 -0300

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,6 +16,16 @@ export default function RootLayout({
   return (
     <html lang="es">
       <head>
+        <meta charSet="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta name="google" content="notranslate" />
+        <meta name="theme-color" content="#fe7630" />
+
+        {/* PWA */}
+        <link rel="manifest" href="/manifest.webmanifest" />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="default" />
+
       </head>
       <body 
       className={fonts.className}>
